bc9e5fa479e13d4c501e1e57987e303cf5ec2e5c
Abbas Ally <abbasally5@yahoo.com>
2014-09-18 00:57:38 -0500

    added codeforces and uva folders

---
+++ uVa/Template.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.lang.*;
+import java.math.*;
+import java.util.regex.*;
+import static java.lang.System.*;
+import static java.util.regex.Pattern.*;
+import static java.util.regex.Matcher.*;
+import static java.math.BigInteger.*;
+import static java.lang.Integer.*;
+import static java.lang.Double.*;
+import static java.util.Collection.*;
+import static java.lang.Math.*;
+import static java.util.Arrays.*;
+import static java.lang.Character.*;
+
+
+public class Template
+{
+    public void run() throws Exception
+    {
+    	Scanner sc = new Scanner(new File(".dat"));
+    	int run = sc.nextInt();
+    	sc.nextLine();
+    	for (int runCount = 1; runCount <= run; runCount++)
+    	{
+
+    	}
+    }
+
+
+    public static void main(String[] args) throws Exception
+    {
+        new Template().run();
+    }
+}
