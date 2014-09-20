ae6312f9c63138b062335c275cebbcda12c0da1f
Abbas Ally <abbasally5@yahoo.com>
2014-09-19 23:06:22 -0500

    Added euler 15

---
+++ Project Euler/euler15.java
@@ -0,0 +1,20 @@
+import static java.lang.System.*;
+
+class euler15
+{
+	void run()
+	{
+		out.println(recur(40,20,1,1));
+	}
+	
+	long recur(long n, long c, int cnt, long num)
+	{
+		if (c <= 0) return num;
+		return recur(n - 1, c - 1, cnt + 1, num * n/cnt);
+	}
+	
+	public static void main(String... args)
+	{
+			new euler15().run();
+	}
+}
